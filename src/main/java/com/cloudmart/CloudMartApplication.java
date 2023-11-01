860846fa030f12e08cc42f5ebde851a4eb3e1c92
s.sharma <sonusharma38151@gmail.com>
2023-11-01 13:04:49 +0530

    first commit

---
+++ src/main/java/com/cloudmart/CloudMartApplication.java
@@ -0,0 +1,13 @@
+package com.cloudmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudmart/CloudMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
